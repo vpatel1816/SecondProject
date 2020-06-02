cf87f1eec930d74aae00fc352e6bb59b2d822e08
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-06-02 10:05:57 -0400

    Delete Function
    Delete In woring Stage

--- CrudApp/BlogPosts/urls.py
+++ CrudApp/BlogPosts/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name= 'home'),
     path('add', views.add, name= 'add'),
-    path("edit/<int:id>", views.edit),
-    path("delete/<int>/id", views.deletee),
+    path("edit/<int:id>", views.edit, name='edit'),
+    path("delete/<int:id>", views.deletee),
+    path("update/<int:id>", views.update, name='update'),
 ]
--- CrudApp/BlogPosts/views.py
+++ CrudApp/BlogPosts/views.py
@@ -7,13 +7,26 @@ def home(request):
 	return render(request,'BlogPosts/home.html', {'blog':blog})
 
 def edit(request, id):
-	blog = Blogs.objects.get(id=id)
-	return render(request, 'BlogPosts/home.html')
+	blogs = Blogs.objects.get(id=id)
+	return render(request,'BlogPosts/edit.html', {'blogs':blogs})
 
 def deletee(request, id):
 	blog = Blogs.objects.get(id=id)
 	blog.delete()
-	return render(request, 'BlogPosts/home.html')
+	return redirect('/')
+
+def update(request, id):
+	if request.method == 'POST':
+		updates = Blogs.objects.get(id=id)
+		blog_title  = request.POST.get('blog_title')
+		blog        = request.POST.get('blog')
+		blog_author = request.POST.get('blog_author') 
+		updates = Blogs(blog_title=blog_title, blog=blog, blog_author=blog_author)
+		updates.save()
+		return redirect('/')
+
+	else:
+		return redirect('www.google.com')
 
 
 def add(request):
