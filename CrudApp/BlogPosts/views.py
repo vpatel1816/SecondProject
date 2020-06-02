bb8b025251e13f4ab3b301aa49ecf9d40dc5f20e
vishall <65184972+vpatel1816@users.noreply.github.com>
2020-06-01 23:46:32 -0400

    Create Blog
    Create Operation is developed

--- CrudApp/BlogPosts/views.py
+++ CrudApp/BlogPosts/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Blogs
 
 # Create your views here.
 def home(request):
 	blog = Blogs.objects.all
 	return render(request,'BlogPosts/home.html', {'blog':blog})
+
+def add(request):
+	if request.method == 'POST':
+		blog_title 	= request.POST.get('blog_title')
+		blog 		= request.POST.get('blog')
+		blog_author = request.POST.get('blog_author')
+		new_blog = Blogs(blog_title=blog_title, blog=blog, blog_author=blog_author)
+		new_blog.save()
+		return redirect('/')
+	else:
+		return render(request, 'BlogPosts/add.html')
